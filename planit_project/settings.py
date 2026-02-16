5db7fa9d1a35f976d7375abc3685d687a35baca9
Babyface Mokoena <mokoenababyface105@gmail.com>
2026-02-16 14:32:33 +0200

    Prepare for Render deployment

--- planit_project/settings.py
+++ planit_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+l)7143@wcxd#@*s^8o0iuqsp-n2-5ba6@s#dbw-erqxrjm^+*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,3 +126,4 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 LOGIN_REDIRECT_URL = 'tasks_list'
 LOGOUT_REDIRECT_URL = 'login'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
